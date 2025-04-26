e979654ed71a7ac26f95be409fc6bf3c40f0a4a8
sabbir-offc <mdsabbirhowlader420@gmail.com>
2025-04-26 23:08:08 +0600

    add google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Poppins } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -63,6 +64,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   {/* Favicon */}
   <link rel="icon" href="/favicon.ico" sizes="any" />
+   {/* Google Analytics */}
+   <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QK3XN9X9GH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QK3XN9X9GH', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
 </head>
 
       <body>
